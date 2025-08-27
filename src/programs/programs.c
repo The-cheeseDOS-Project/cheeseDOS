79ebb1359e9c30bbe23143cc86423e4c604695e5
Connor <connor@ThinkPad-T480s.attlocal.net>
2025-08-27 00:47:50 -0700

    Make mem say like 58K / 640K

--- src/programs/programs.c
+++ src/programs/programs.c
@@ -1524,15 +1524,6 @@ static void hey(const char* args) {
     }
 }
 
-static void mem(const char*) {
-    uint32_t used = heap_ptr - (uint32_t)&_kernel_start;
-    uint32_t used_kb = used / 1024;
-    char buf[16];
-    itoa(used_kb, buf, 10);
-    print(buf);
-    print("K RAM USED\n");
-}
-
 static void ban(const char*) {
     const uint8_t glyphs[][8] = {
         {0x3C,0x40,0x80,0x80,0x80,0x80,0x40,0x3C},
@@ -1625,6 +1616,49 @@ static void ban(const char*) {
     }
 
     clear_screen();
+}
+
+static void ram_used() {
+    uint32_t used = heap_ptr - (uint32_t)&_kernel_start;
+    uint32_t used_kb = used / 1024;
+    char buf[16];
+    itoa(used_kb, buf, 10);
+    print(buf);
+    print("K");
+}
+
+static void ram_allocated() {
+    unsigned char low, high;
+    unsigned short ext_kb, total_kb, total_mb;
+
+    outb(0x70, 0x30); low = inb(0x71);
+    outb(0x70, 0x31); high = inb(0x71);
+
+    ext_kb = ((unsigned short)high << 8) | low;
+    total_kb = 640 + ext_kb;
+    total_mb = total_kb / 1024;
+
+    unsigned short val = total_kb;
+    unsigned short div = 10000;
+    int started = 0;
+    while (div > 0) {
+        char digit = (val / div) % 10;
+        if (digit || started || div == 1) {
+            char c[2] = { '0' + digit, '\0' };
+            print(c);
+            started = 1;
+        }
+        div /= 10;
+    }
+
+    print("K");
+}
+
+static void mem(const char*) {
+    ram_used();
+    print(" / ");
+    ram_allocated();
+    print("\n");
 }
 
 static shell_command_t commands[] = {
