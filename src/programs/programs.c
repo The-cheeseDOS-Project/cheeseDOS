3b2b497c941be0b88a72104b3d85483c8e45f337
Connor <connor@ThinkPad-T480s.attlocal.net>
2025-08-27 16:55:40 -0700

    Fix ascii garbage on ver and boot

--- src/programs/programs.c
+++ src/programs/programs.c
@@ -106,6 +106,7 @@ static void hlp(const char*) {
 static void ver(const char*) {
     print("cheeseDOS version ");
     print(_binary_src_version_version_txt_start);
+    print("\n");
 }
 
 static void hi(const char*) {
