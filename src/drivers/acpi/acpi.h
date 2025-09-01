61a9d60924d008c0b3429f63ccf8abc1bf425016
GNUfault <bluMATRIKZ@gmail.com>
2025-08-31 17:39:56 -0700

    Strip down `acpi.h`

--- src/drivers/acpi/acpi.h
+++ src/drivers/acpi/acpi.h
@@ -15,11 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
- 
-#ifndef CHEESEDOS_ACPI_H
-#define CHEESEDOS_ACPI_H
 
-#include "stdint.h"
+#ifndef ACPI_H
+#define ACPI_H
 
 void shutdown(void);
 void reboot(void);
