9d45ad7b114f90f29f96ee84a2bcb9fc40d015e5
GNUfault <bluMATRIKZ@gmail.com>
2025-09-17 17:15:01 -0700

    More small

--- strip.c
+++ strip.c
@@ -1,6 +1,6 @@
 /*
  * cheeseDOS - My x86 DOS
- * Copyright (C) 2025  Connor Thomson
+ * Copyright (C) 2025 Connor Thomson
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -9,11 +9,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
@@ -27,6 +27,23 @@
 #include <elf.h>
 
 #define TARGET_PATH "build/cheesedos.elf"
+
+int is_strippable(const char *name) {
+    return strcmp(name, ".comment") == 0 ||
+           strcmp(name, ".note") == 0 ||
+           strncmp(name, ".debug", 6) == 0 ||
+           strcmp(name, ".symtab") == 0 ||
+           strcmp(name, ".strtab") == 0 ||
+           strncmp(name, ".rel", 4) == 0 ||
+           strncmp(name, ".rela", 5) == 0 ||
+           strcmp(name, ".eh_frame") == 0 ||
+           strncmp(name, ".stab", 5) == 0 ||
+           strcmp(name, ".gdb_index") == 0 ||
+           strcmp(name, ".plt") == 0 ||
+           strcmp(name, ".got") == 0 ||
+           strcmp(name, ".init") == 0 ||
+           strcmp(name, ".fini") == 0;
+}
 
 int main() {
     int fd = open(TARGET_PATH, O_RDWR);
@@ -57,13 +74,32 @@ int main() {
         return 1;
     }
 
+    memset(ehdr->e_ident, 0, EI_NIDENT);
+
+    Elf32_Shdr *shdr = (Elf32_Shdr *)(data + ehdr->e_shoff);
+    const char *shstrtab = (const char *)(data + shdr[ehdr->e_shstrndx].sh_offset);
+
+    for (int i = 0; i < ehdr->e_shnum; i++) {
+        const char *name = shstrtab + shdr[i].sh_name;
+        if (is_strippable(name)) {
+            memset(data + shdr[i].sh_offset, 0, shdr[i].sh_size);
+        }
+    }
+
     Elf32_Phdr *phdr = (Elf32_Phdr *)(data + ehdr->e_phoff);
     Elf32_Off max_offset = 0;
 
     for (int i = 0; i < ehdr->e_phnum; i++) {
+        if (phdr[i].p_type != PT_LOAD) {
+            memset(&phdr[i], 0, sizeof(Elf32_Phdr));
+        }
+    }
+
+    for (int i = 0; i < ehdr->e_phnum; i++) {
         if (phdr[i].p_type == PT_LOAD) {
             Elf32_Off end = phdr[i].p_offset + phdr[i].p_filesz;
-            if (end > max_offset) max_offset = end;
+            if (end > max_offset)
+                max_offset = end;
         }
     }
 
@@ -80,6 +116,5 @@ int main() {
 
     munmap(data, st.st_size);
     close(fd);
-
     return 0;
 }
