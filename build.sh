efb0d082388a654b37d44eb04cadea5172f61e1a
GNUfault <bluMATRIKZ@gmail.com>
2025-10-15 21:34:32 -0700

    Set -e on build.sh

--- build.sh
+++ build.sh
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+set -e
+
 if [ -f config.conf ]; then
     . ./config.conf
   else
