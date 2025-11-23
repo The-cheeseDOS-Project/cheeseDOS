b1e4e2e3363f64709eeaf9ebfcd06f626b380792
GNUfault <bluMATRIKZ@gmail.com>
2025-11-23 14:07:53 -0800

    Add kernel loading

--- src/boot/stage2.asm
+++ src/boot/stage2.asm
@@ -7,6 +7,8 @@ stage2:
     mov ss, ax
     mov sp, 0x7C00
     sti
+
+    mov [BootDrive], dl
     
     mov ah, 0x02
     mov bh, 0x00
@@ -36,10 +38,10 @@ pmode:
     mov eax, cr0
     and eax, 0xFFFFFFFE
     mov cr0, eax
-    jmp 0x0:huge_unreal
+    jmp 0x0:unreal
     
 [BITS 16]
-huge_unreal:
+unreal:
     pop ds
     xor ax, ax
     mov es, ax
@@ -49,8 +51,7 @@ huge_unreal:
     sti
     mov si, entering_unreal
     call print
-    mov si, hello_msg
-    call print
+    jmp boot
     jmp $
     
 print:
@@ -65,11 +66,49 @@ print:
 .done:
     ret
 
+boot:
+    mov si, loading_disk
+    call print
+
+    mov bx, 0
+    mov ax, 0x1000
+    mov es, ax
+    mov dl, [BootDrive]
+    mov dh, 0
+    mov ch, 0
+    mov cl, 2
+
+read:
+    mov ah, 2
+    mov al, 1
+    int 0x13
+    jc done
+    mov ax, [es:bx]
+    cmp ax, 0
+    je done
+    add bx, 512
+    inc cl
+    jmp read
+
+done:
+    mov si, hello_msg
+    call print
+
+halt:
+    cli
+    hlt
+
+.ret:
+    ret
+
+loading_disk db "Loading kernel...",0x0D, 0x0A, 0
 loading_gdt db "Loading GDT...", 0x0D, 0x0A, 0
 enter_pmode db "Entering Protected Mode...", 0x0D, 0x0A, 0
 entering_unreal db "Entering Unreal mode...", 0x0D, 0x0A, 0
 hello_msg db "hello, world!",0x0D,0x0A,0
-    
+
+BootDrive db 0
+
 align 8
 gdt:
     dd 0,0
