9ce40e6d26883db8b0bf747fd3eef77ece54d19b
Connor Thomson <blumatrikz@gmail.com>
2025-11-24 17:00:44 -0800

    Add GPL Header

--- src/boot/stage2.asm
+++ src/boot/stage2.asm
@@ -1,5 +1,22 @@
+; cheeseDOS - My x86 DOS
+; Copyright (C) 2025  Connor Thomson
+;
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ORG 0x7E00
-BITS 16
+
+[BITS 16]
 stage2:
     cli
     xor ax, ax
