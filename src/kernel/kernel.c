a667ae3794d93c22811198a134447c2ef7b86726
GNUfault <bluMATRIKZ@gmail.com>
2025-11-25 00:08:26 -0800

    Fix error

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,18 +1,7 @@
 #include "vga.h"
-#include "keyboard.h"
 #include "cpu.h"
 
 int kmain(void) {
     print("Kernel started!");
-    keyboard_init(key_pressed);
-
-    sti();
-
-    while (1) {
-        if (last_key) {
-            char c = last_key;
-            last_key = 0;
-            putchar(c);
-        }
-    }
+    hlt();
 }
